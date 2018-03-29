9021db2dbe80dd7f95b640c9236e4ab52da5b531
John Eskew <jeskew@edx.org>
2018-03-29 12:54:09 -0400

    Add click script to retire usernames/emails.

--- user_util/cli.py
+++ user_util/cli.py
@@ -1,18 +1,46 @@
 # -*- coding: utf-8 -*-
 
 """Console script for user_util."""
+import json
 import sys
 import click
+from . import user_util
 
 
 @click.command()
-def main(args=None):
-    """Console script for user_util."""
-    click.echo("Replace this message by putting your code into "
-               "user_util.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.option('--username', '-u',
+              help='Original username to convert to retired username. Multiple usernames can be specified.',
+              multiple=True
+              )
+@click.option('--email', '-e',
+              help='Original email address to convert to retired email address. '
+                   'Multiple email addresses can be specified.',
+              multiple=True
+              )
+@click.option('--salt', '-s',
+              help='JSON-formatted list of salt strings ordered from oldest to current. '
+                   'All double-quotes must be escaped.'
+              )
+def retire_user(username, email, salt):
+    """
+    Console script for user_util to convert usernames/email addresses to retired usernames/email addresses.
+    """
+    try:
+        salt_list = json.loads(salt)
+    except TypeError, ValueError:
+        click.echo("Salt value \"{}\" is invalid JSON.".format(salt))
+        raise
+    results = {}
+    for name in username:
+        # results[name] = get_retired_username(name, salt_list)
+        results[name] = user_util.get_retired_username(name, salt_list)
+    for email_addr in email:
+        # results[email_addr] = get_retired_email(email_addr, salt_list)
+        results[email_addr] = user_util.get_retired_email(email_addr, salt_list)
+
+    click.echo("{}".format(json.dumps(results)))
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    sys.exit(retire_user())  # pragma: no cover
