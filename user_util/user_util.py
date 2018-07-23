33e45394e4e6735dd1e248486b1562255be150f8
Troy Sankey <sankeytms@gmail.com>
2018-07-23 15:06:52 -0400

    Lowercase emails before hashing.
    This resolves an issue where account registrations with retired email
    addresses are not blocked when the email case is different (but the
    email is still the same).
    
    PLAT-2254

--- user_util/user_util.py
+++ user_util/user_util.py
@@ -47,9 +47,9 @@ def get_all_retired_usernames(username, salt_list, retired_username_fmt=RETIRED_
 
 def get_all_retired_emails(email, salt_list, retired_email_fmt=RETIRED_EMAIL_DEFAULT_FMT):
     """
-    Returns a generator of possible retired email addresses based on the original email
-    and all the historical salts, from oldest to current.
-    The current salt is assumed to be the last salt in the list.
+    Returns a generator of possible retired email addresses based on the
+    original lowercase email and all the historical salts, from oldest to
+    current.  The current salt is assumed to be the last salt in the list.
 
     Raises :class:`~ValueError` if the salt isn't a list of salts.
 
@@ -65,7 +65,7 @@ def get_all_retired_emails(email, salt_list, retired_email_fmt=RETIRED_EMAIL_DEF
         raise SALT_LIST_EXCEPTION
 
     for salt in salt_list:
-        yield retired_email_fmt.format(_compute_retired_hash(email, salt))
+        yield retired_email_fmt.format(_compute_retired_hash(email.lower(), salt))
 
 
 def get_retired_username(username, salt_list, retired_username_fmt=RETIRED_USERNAME_DEFAULT_FMT):
@@ -92,11 +92,11 @@ def get_retired_username(username, salt_list, retired_username_fmt=RETIRED_USERN
 
 def get_retired_email(email, salt_list, retired_email_fmt=RETIRED_EMAIL_DEFAULT_FMT):
     """
-    Returns a retired email address based on the original email address
-    and all the historical salts, from oldest to current.
-    The current salt is assumed to be the last salt in the list.
+    Returns a retired email address based on the original lowercase email
+    address and the current salt. The current salt is assumed to be the last
+    salt in the list.
 
-    Raises :class:`~ValueError` if the salt isn't a list of salts.
+    Raises :class:`~ValueError` if salt_list isn't a list of salts.
 
     Arguments:
         email (str): Email address of the user to be retired.
@@ -104,9 +104,9 @@ def get_retired_email(email, salt_list, retired_email_fmt=RETIRED_EMAIL_DEFAULT_
 
     Yields:
         Returns a retired email address based on the original email
-        and all the historical salts, including the current salt.
+        and the current salt
     """
     if not isinstance(salt_list, (list, tuple)):
         raise SALT_LIST_EXCEPTION
 
-    return retired_email_fmt.format(_compute_retired_hash(email, salt_list[-1]))
+    return retired_email_fmt.format(_compute_retired_hash(email.lower(), salt_list[-1]))
