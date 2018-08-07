10aa05da5a0d373dbbf2d48aca17811fc21e755f
Troy Sankey <sankeytms@gmail.com>
2018-08-07 10:39:09 -0400

    Normalize (lowercase) username before hashing
    PLAT-2287

--- user_util/user_util.py
+++ user_util/user_util.py
@@ -24,8 +24,8 @@ def _compute_retired_hash(value_to_retire, salt):
 
 def get_all_retired_usernames(username, salt_list, retired_username_fmt=RETIRED_USERNAME_DEFAULT_FMT):
     """
-    Returns a generator of possible retired usernames based on the original username
-    and all the historical salts, from oldest to current.
+    Returns a generator of possible retired usernames based on the original
+    lowercased username and all the historical salts, from oldest to current.
     The current salt is assumed to be the last salt in the list.
 
     Raises :class:`~ValueError` if the salt isn't a list of salts.
@@ -42,13 +42,13 @@ def get_all_retired_usernames(username, salt_list, retired_username_fmt=RETIRED_
         raise SALT_LIST_EXCEPTION
 
     for salt in salt_list:
-        yield retired_username_fmt.format(_compute_retired_hash(username, salt))
+        yield retired_username_fmt.format(_compute_retired_hash(username.lower(), salt))
 
 
 def get_all_retired_emails(email, salt_list, retired_email_fmt=RETIRED_EMAIL_DEFAULT_FMT):
     """
     Returns a generator of possible retired email addresses based on the
-    original lowercase email and all the historical salts, from oldest to
+    original lowercased email and all the historical salts, from oldest to
     current.  The current salt is assumed to be the last salt in the list.
 
     Raises :class:`~ValueError` if the salt isn't a list of salts.
@@ -70,9 +70,9 @@ def get_all_retired_emails(email, salt_list, retired_email_fmt=RETIRED_EMAIL_DEF
 
 def get_retired_username(username, salt_list, retired_username_fmt=RETIRED_USERNAME_DEFAULT_FMT):
     """
-    Returns a retired username based on the original username
-    and all the historical salts, from oldest to current.
-    The current salt is assumed to be the last salt in the list.
+    Returns a retired username based on the original lowercased username and
+    all the historical salts, from oldest to current.  The current salt is
+    assumed to be the last salt in the list.
 
     Raises :class:`~ValueError` if the salt isn't a list of salts.
 
@@ -87,12 +87,12 @@ def get_retired_username(username, salt_list, retired_username_fmt=RETIRED_USERN
     if not isinstance(salt_list, (list, tuple)):
         raise SALT_LIST_EXCEPTION
 
-    return retired_username_fmt.format(_compute_retired_hash(username, salt_list[-1]))
+    return retired_username_fmt.format(_compute_retired_hash(username.lower(), salt_list[-1]))
 
 
 def get_retired_email(email, salt_list, retired_email_fmt=RETIRED_EMAIL_DEFAULT_FMT):
     """
-    Returns a retired email address based on the original lowercase email
+    Returns a retired email address based on the original lowercased email
     address and the current salt. The current salt is assumed to be the last
     salt in the list.
 
