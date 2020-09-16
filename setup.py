eda332de5b2dc7a30a80f6c2e024a95aefec7fb4
Simon Chen <schen@edx.org>
2020-09-16 16:41:01 -0400

    Fix the version of this library to the proper version 0.3.1

--- setup.py
+++ setup.py
@@ -68,6 +68,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/edx/user-util',
-    version='0.2',
+    version='0.3.1',
     zip_safe=False,
 )
--- user_util/__init__.py
+++ user_util/__init__.py
@@ -2,4 +2,4 @@
 
 """Top-level package for Open edX User Utilities."""
 
-__version__ = '0.1.5'
+__version__ = '0.3.1'
